9baa5a656b1ac94895a38b5ae7845c6476cb551a
Katie Hoffman <ktahoffman@gmail.com>
2014-12-04 14:48:53 -0500

    add passing code

--- lib/format_minutes.rb
+++ lib/format_minutes.rb
@@ -1,3 +1,14 @@
+MESSAGES = [" hour(s)", " minute(s)"]
+
 def time_formatter(minutes)
-  # your code here
+  message = []
+  hours_and_min = [(minutes / 60), (minutes % 60)]
+  
+  i = 0
+  hours_and_min.each do |v| 
+    message << v.to_s + MESSAGES[i] if v > 0
+    i += 1
+  end
+  
+  message.join(", ")
 end
