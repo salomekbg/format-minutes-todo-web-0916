a0807c73f1fe308f8806655e4050ecb4fff9cc47
Katie Hoffman <ktahoffman@gmail.com>
2014-12-12 14:56:53 -0500

    update

--- lib/format_minutes.rb
+++ lib/format_minutes.rb
@@ -1,14 +1,8 @@
 MESSAGES = [" hour(s)", " minute(s)"]
 
-def time_formatter(minutes)
-  message = []
-  hours_and_min = [(minutes / 60), (minutes % 60)]
-  
-  i = 0
-  hours_and_min.each do |v| 
-    message << v.to_s + MESSAGES[i] if v > 0
-    i += 1
-  end
-  
-  message.join(", ")
+def time_formatter(mins)
+  hr_min = [(mins / 60), (mins % 60)]
+  hr_min.collect.with_index do |time, i| 
+    time > 0 ? time.to_s + MESSAGES[i] : nil
+  end.compact.join(", ")
 end
