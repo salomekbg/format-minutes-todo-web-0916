2cf2879bf4594967c0277de71d65616a38c3e0e9
SluvsP <salomekbg@gmail.com>
2017-01-11 18:10:36 -0500

    Done.

--- lib/format_minutes.rb
+++ lib/format_minutes.rb
@@ -1,3 +1,9 @@
 def time_formatter(minutes)
-  # your code here
+  if minutes < 60
+    return "#{minutes.to_s} minute(s)"
+  else
+    hours = minutes / 60
+    left_minutes = minutes % 60
+    left_minutes > 0 ? "#{hours.to_s} hour(s), #{left_minutes.to_s} minute(s)" : "#{hours.to_s} hour(s)"
+  end
 end
